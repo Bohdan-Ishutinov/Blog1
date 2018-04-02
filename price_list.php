2ceb26785b4abadc753235fefabf7c6e04f69081
karbon25 <karbon.tv66@gmail.com>
2018-04-02 21:49:43 +0300

    new file

---
+++ price_list.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Контур.Студия мебели</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/price_list.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<header class="header">
+		<img src="img/logo.png">	
+		</header>
+
+
+		<nav class="navbar nav navbar-default">
+		  
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-main">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		    <div class="collapse navbar-collapse" id="navbar-main">
+		      <ul class="nav navbar-nav">
+		        <li><a href="index.php">Главная</a></li>
+		        <li class="active"><a href="price_list.php">Прайс лист</a></li>
+		        <li><a href="gallery.php">Галерея</a></li>
+		        <li><a href="contacts.php">Контакты</a></li>
+		        <li><a href="issues.php">Вопросы</a></li>
+		      </ul>
+		    </div>
+		  </div>
+		</nav>
+	
+		<div class="container">
+			<h1 id="title">Прайс листы</h1>
+			<table class="table table-bordered">
+				<tr>
+					<td>Мягкая мебель</td>
+					<td><a href="document/m_mebel.xlsx">Скачать прайс</a></td>
+				</tr>
+				<tr>
+					<td>Шкафи</td>
+					<td><a href="document/shcaf_mebel.xlsx">Скачать прайс</a></td>
+				</tr>
+				<tr>
+					<td>Столы</td>
+					<td><a href="document/stol_mebel.xlsx">Скачать прайс</a></td>
+				</tr>
+				<tr>
+					<td>Кухни</td>
+					<td><a href="document/kyxna_mebel.xlsx">Скачать прайс</a></td>
+				</tr>
+
+
+			</table>
+		</div>
+
+		<footer class="footer">
+			<div class="container">
+  				<p><a href="https://www.facebook.com/profile.php?id=100017194929576">Karbon</a></p>
+  				<p>© Copyright 2018. All rights reserved</p>
+			</div>
+		</footer>
+	</body>
+</html>
