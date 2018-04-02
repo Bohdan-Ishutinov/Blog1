090dbb6d2145a1b3d99f25b5508a4a85adf3eeed
karbon25 <karbon.tv66@gmail.com>
2018-04-03 01:51:05 +0300

    попытка №2

--- rsc/sign_in.php
+++ rsc/sign_in.php
@@ -14,4 +14,4 @@ if($_POST["sign_in"]){
 	echo "<script> window.location.href = 'index.php'</script>";
 }
 echo "<script> window.location.href = 'index.php'</script>";
-// ето такая легкая поибня, что и вазилина не нужно?>
+?>
