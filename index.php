81453d94f786d71843c04540cd62062e1b5328ff
Bohdan-Ishutinov <Bohdan-Ishutinov@users.noreply.github.com>
2018-04-02 19:59:14 +0300

    update index

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<title></title>
+</head>
+<body>
+	<div class="container"></div>
+
+
+
+
+
+   <footer class="footer">
+			<div class="container">
+  				<p><a href="https://www.facebook.com/profile.php?id=100017194929576">Karbon</a></p>
+  				<p>© Copyright 2018. All rights reserved</p>
+			</div>
+		</footer>
+</body>
+</html>
