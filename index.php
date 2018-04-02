c11c0eaf7ad1e05d543d24a5bf7577f6f657dc06
Bohdan-Ishutinov <Bohdan-Ishutinov@users.noreply.github.com>
2018-04-02 20:11:25 +0300

    updateindex

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<title></title>
 </head>
 <body>
-	<div class="col-md-1" style="background-color: black; height: 200px"></div>
+	<div class="col-md-1"></div>
+	<div class="col-md-10"></div>
+	<div class="col-md-1"></div>
 
 
 
