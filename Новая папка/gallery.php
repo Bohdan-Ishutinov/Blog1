2bb34c26645c06233a5de7a1d81af0903d17d297
karbon25 <karbon.tv66@gmail.com>
2018-04-02 21:29:43 +0300

    123

---
+++ Новая папка/gallery.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Контур.Студия мебели</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/gallery.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<header class="header">
+		<img src="img/logo.png">	
+		</header>
+
+
+		<nav class="navbar nav navbar-default">
+		  
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-main">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		    <div class="collapse navbar-collapse" id="navbar-main">
+		      <ul class="nav navbar-nav">
+		        <li><a href="index.php">Главная</a></li>
+		        <li><a href="price_list.php">Прайс лист</a></li>
+		        <li class="active"><a href="gallery.php">Галерея</a></li>
+		        <li><a href="contacts.php">Контакты</a></li>
+		        <li><a href="issues.php">Вопросы</a></li>
+		      </ul>
+		    </div>
+		  </div>
+		</nav>
+
+		<div class="container">
+			<div id="carousel">
+				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				  <ol class="carousel-indicators">
+				    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				  </ol>
+
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner" role="listbox">
+				    <div class="item active">
+				      <img src="img/gallery/1.jpg" width="100%">
+				      <div class="carousel-caption">
+				      </div>
+				    </div>
+				    <div class="item">
+				      <img src="img/gallery/2.jpg" width="100%">
+				      <div class="carousel-caption">
+				      </div>
+				    </div>
+				    ...
+				  </div>
+
+				  <!-- Controls -->
+				  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			
+
+
+
+		</div>
+		<footer class="footer">
+			<div class="container">
+  				<p><a href="https://www.facebook.com/profile.php?id=100017194929576">Karbon</a></p>
+  				<p>© Copyright 2018. All rights reserved</p>
+			</div>
+		</footer>
+	</body>
+</html>
---
+++ Новая папка/index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Контур.Студия мебели</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<header class="header">
+		<img src="img/logo.png">	
+		</header>
+
+
+		<nav class="navbar nav navbar-default">
+		  
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-main">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		    <div class="collapse navbar-collapse" id="navbar-main">
+		      <ul class="nav navbar-nav">
+		        <li class="active"><a href="index.php">Главная</a></li>
+		        <li><a href="price_list.php">Прайс лист</a></li>
+		        <li><a href="gallery.php">Галерея</a></li>
+		        <li><a href="contacts.php">Контакты</a></li>
+		        <li><a href="issues.php">Вопросы</a></li>
+		      </ul>
+		    </div>
+		  </div>
+		</nav>
+		<div class="body_h">
+			<h1>Контур - студия мебели</h1>
+			<h3>Дизайнерская мебель от производителя</h3>	
+		</div>
+		<div class="container body_c">
+			<div class="row">
+				<h1 style="text-align: center;">Под заказ</h1>
+				<br>
+			  <div class="col-md-4">
+			    <div class="thumbnail">	<br>
+			      <img src="img/photo_1.jpg">
+			      <div class="caption">
+			        <h3>Кухны</h3>
+			        <p>...</p>
+			        <p><a href="#" class="btn btn-primary" role="button">Смотреть в галерее</a></p>
+			      </div>
+			    </div>
+			  </div>
+
+
+			  <div class="col-md-4">
+			    <div class="thumbnail">	<br>
+			      <img src="img/photo_2.jpg">
+			      <div class="caption">
+			        <h3>Шкафы</h3>
+			        <p>...</p>
+			        <p><a href="#" class="btn btn-primary" role="button">Смотреть в галерее</a></p>
+			      </div>
+			    </div>
+			  </div>
+
+
+			  <div class="col-md-4">
+			    <div class="thumbnail">	<br>
+			      <img src="img/photo_3.jpg">
+			      <div class="caption">
+			        <h3>Столы</h3>
+			        <p>...</p>
+			        <p><a href="#" class="btn btn-primary" role="button">Смотреть в галерее</a></p>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+			<div class="row">
+				<br>
+				<h1 style="text-align: center;">Готовые изделия</h1>
+				<br>
+			  <div class="col-md-4">
+			    <div class="thumbnail">	<br>
+			      <img src="img/photo_1.jpg">
+			      <div class="caption">
+			        <h3>Кухоная мебель</h3>
+			        <p>...</p>
+			        <p><a href="#" class="btn btn-primary" role="button">Смотреть в галерее</a></p>
+			      </div>
+			    </div>
+			  </div>
+
+
+			  <div class="col-md-4">
+			    <div class="thumbnail">	<br>
+			      <img src="img/photo_2.jpg">
+			      <div class="caption">
+			        <h3>Мягкая мебель</h3>
+			        <p>...</p>
+			        <p><a href="#" class="btn btn-primary" role="button">Смотреть в галерее</a></p>
+			      </div>
+			    </div>
+			  </div>
+
+
+			  <div class="col-md-4">
+			    <div class="thumbnail">	<br>
+			      <img src="img/photo_3.jpg">
+			      <div class="caption">
+			        <h3>Письменные столы</h3>
+			        <p>...</p>
+			        <p><a href="#" class="btn btn-primary" role="button">Смотреть в галерее</a></p>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+		</div>
+		<footer class="footer">
+			<div class="container">
+  				<p><a href="https://www.facebook.com/profile.php?id=100017194929576">Karbon</a></p>
+  				<p>© Copyright 2018. All rights reserved</p>
+			</div>
+		</footer>
+	</body>
+</html>
