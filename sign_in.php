2b28cf30df54f714a60d9b5804a97caca3b6eb81
karbon25 <karbon.tv66@gmail.com>
2018-04-04 10:13:22 +0300

    1

---
+++ sign_in.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<div id="header">
+	<div class="title_header">
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-8"></div>
+		</div>
+
+	</div>
+
+
+
+</div>
+</body>
+<style>
+#header{
+	height: 250px;
+	background-color: #022e3a;
+	color: #fff;
+}
+#header h1{
+	margin-top: 90px;
+}	
+.nav{
+	background-color: #022e3a;
+	width: 100%
+}
+
+
+</style>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+</html>
